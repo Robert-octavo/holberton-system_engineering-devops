68499ad550eb211181a45a1e15499eee6748051d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-23 12:03:57 -0500

    another test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,10 @@
 # Change SSH config file
 exec { 'file':
-  command  => '/urs/bin/echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command  => '/urs/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  provider => 'shell',
+}
+
+exec { 'file2':
+  command  => '/urs/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   provider => 'shell',
 }
