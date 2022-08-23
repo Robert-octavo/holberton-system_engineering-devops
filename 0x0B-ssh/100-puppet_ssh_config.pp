4cc612cb78b9419fe2cfefbc2312e7d043927486
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-23 12:28:09 -0500

    correct path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,6 @@ file {'/etc/ssh/ssh_config':
   ensure => 'present',
 }
 exec { 'file':
-  command  => '/urs/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-  provider => 'shell',
-}
-
-exec { 'file2':
-  command  => '/urs/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  command  => '/usr/bin/echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   provider => 'shell',
 }
