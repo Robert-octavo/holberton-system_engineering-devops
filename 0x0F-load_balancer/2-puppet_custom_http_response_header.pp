d57a1f2361e0d590f08ba231ae7175bc04ddd61b
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-08-24 20:07:55 -0500

    test puppet just add the custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# to install nginx
+# exec {'install':
+#  provider => shell,
+#  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo service nginx start',
+# }
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'custom_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => 'add_header X-Served-By $HOSTNAME;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
