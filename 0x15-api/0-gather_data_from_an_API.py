a3e7246c654ebdc66738f1ee7c51890f56cc6f84
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-08 19:10:04 -0500

    Use a Rest Api, return his/her ccompleted task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/urs/bin/python3
+"""script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f'users/{sys.argv[1]}').json()
+    # print(user)
+    tasks = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    # print(tasks)
+    complete = []
+    for task in tasks:
+        if task.get('completed'):
+            complete.append(task.get('title'))
+    # print(f'{len(tasks)}:{len(complete)}')
+    # print(complete)
+    print(f'Employee {user.get("name")} is done with \
+tasks({len(complete)}/{len(tasks)}):')
+    [print(f'\t {i}') for i in complete]
