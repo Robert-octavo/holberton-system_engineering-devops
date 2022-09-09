720e138510863a1d21464e9b9968643380d8efec
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-09 09:20:30 -0500

    change to .format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,7 @@ information about his/her TODO list progress
 import requests
 import sys
 
+
 if __name__ == '__main__':
     """script that, using this REST API, for a given employee ID,
     returns information about his/her TODO list progress
@@ -20,6 +21,6 @@ if __name__ == '__main__':
             complete.append(task.get('title'))
     # print(f'{len(tasks)}:{len(complete)}')
     # print(complete)
-    print(f'Employee {user.get("name")} is done with \
-tasks({len(complete)}/{len(tasks)}):')
-    [print(f'\t {i}') for i in complete]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(user.get("name"), len(complete), len(tasks)))
+    [print('\t {}'.format(i)) for i in complete]
