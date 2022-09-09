998d57b1bb2900e860cc0c20f7cd4d58fcb92d2f
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-08 22:29:25 -0500

    Python script to export data in the JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/urs/bin/python3
+"""Python script to export data in the JSON format
+"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + f'users/{sys.argv[1]}').json()
+    # print(user)
+    tasks = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    # print(tasks)
+    info_tasks = []
+    for task in tasks:
+        disct = {'task': task.get('title'),
+                 'completed': task.get('completed'), 
+                 'username': user.get('name')}
+        info_tasks.append(disct)
+    disct = {f'{sys.argv[1]}': info_tasks}
+    # print(disct)
+
+    with open(f'{sys.argv[1]}.json', 'w', encoding='UTF8', newline='') as f:
+        json.dump(disct, f)
